8b794358bfae85a98cf54b6eadb15ace040c82b6
muh rifai <48577618+muhrifai7@users.noreply.github.com>
2020-11-10 17:50:39 +0700

    add

---
+++ migrations/20201110072604-posts.js
@@ -0,0 +1,37 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     */
+    return queryInterface.createTable("posts", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      published: {
+        type: Sequelize.BOOLEAN,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
