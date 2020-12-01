643f38ea8bfac56dd0303c39c17784951270761e
muh rifai <48577618+muhrifai7@users.noreply.github.com>
2020-12-02 02:32:07 +0700

    add postgres

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,4 @@
 // module.exports = {
-//   /*
-//    * First five parameters are for MySQL connection.
-//    */
 //   HOST: "localhost",
 //   USER: "root",
 //   PASSWORD: "",
@@ -14,24 +11,6 @@
 //     idle: 10000, // maximum time, in milliseconds, that a connection can be idle before being released
 //   },
 // };
-
-// module.exports = {
-//   /*
-//    * First five parameters are for MySQL connection.
-//    */
-//   HOST: "ec2-34-204-121-199.compute-1.amazonaws.com",
-//   USER: "ozcvvcabedtkrz",
-//   PASSWORD: "cd2648cd0f73083c3a649a47b21c97b14c56c67aae03bc07aa6059aeb8eb403e",
-//   DB: "dc5b0cklvojpur",
-//   dialect: "mysql",
-//   pool: {
-//     max: 5, // maximum number of connection in pool
-//     min: 0, // minimum number of connection in pool
-//     acquire: 30000, // maximum time, in milliseconds, that pool will try to get connection before throwing error
-//     idle: 10000, // maximum time, in milliseconds, that a connection can be idle before being released
-//   },
-// };
-
 module.exports = {
   HOST: "localhost",
   USER: "postgres",
@@ -45,14 +24,3 @@ module.exports = {
     idle: 10000,
   },
 };
-
-// INSERT INTO posts (title, subtitle, content, published)
-// VALUES ('Raju', 'Kumar', '1996-12-02', true)
-
-// CREATE TABLE posts(
-//       id SERIAL PRIMARY KEY,
-//     title VARCHAR (500),
-//     subtitle VARCHAR (1200),
-// 	description varchar(10000),
-// 	published BOOLEAN NOT NULL
-// );
--- app/models/index.js
+++ app/models/index.js
@@ -2,16 +2,15 @@ const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    operatorAliases: false,
-
-    pool: {
-        max: dbConfig.pool.max,
-        min: dbConfig.pool.min,
-        acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.idle
-    }
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+  operatorAliases: false,
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle,
+  },
 });
 
 const db = {};
@@ -20,6 +19,5 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 db.posts = require("./post.model.js")(sequelize, Sequelize);
-
 
 module.exports = db;
